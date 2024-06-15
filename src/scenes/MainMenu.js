5bd4f0d92fe573f34a86af792ee3c970986fff81
Sagittarius87 <Jarik-87@yandex.ru>
2024-06-16 01:13:38 +0300

    В MainMenuButtons.js создал подклассы ButtonNewGame, ButtonContinueGame, ButtonGameSettings, ButtonGameExit для реализации кнопок главного меню. В MainMenu.js добавил на сцену кнопки главного меню с помощью классов.

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -1,5 +1,5 @@
 import { Scene } from 'phaser';
-import { MainMenuButtons } from './MainMenuButtons';
+import { ButtonContinueGame, ButtonGameExit, ButtonGameSettings, ButtonNewGame, MainMenuButtons } from './MainMenuButtons';
 
 export class MainMenu extends Scene
 {
@@ -16,10 +16,16 @@ export class MainMenu extends Scene
             fontFamily: 'Arial Black', fontSize: 18, color: '#ffffff', align: 'center'
         }).setOrigin(0.5);
 
-        const button = new MainMenuButtons(this, 128, 112);
-        this.add.existing(button);
-
-        
+        //const button = new MainMenuButtons(this, 128, 112);
+        //this.add.existing(button);
+        const buttonNewGame = new ButtonNewGame(this, 128, 112);
+        this.add.existing(buttonNewGame);
+        const buttonContinueGame = new ButtonContinueGame(this, 128, 112 + 32);
+        this.add.existing(buttonContinueGame);
+        const buttonGameSettings = new ButtonGameSettings(this, 128, 112 + 64);
+        this.add.existing(buttonGameSettings);
+        const buttonGameExit = new ButtonGameExit(this, 128, 112 + 96);
+        this.add.existing(buttonGameExit);
 
         /*
         this.input.once('pointerdown', () => {
--- src/scenes/MainMenuButtons.js
+++ src/scenes/MainMenuButtons.js
@@ -5,7 +5,7 @@ export class MainMenuButtons extends Phaser.GameObjects.Container
     buttonTexture = 'buttonImage';
     buttonTextureMove = 'buttonImageMove';
     buttonTextureClick = 'buttonImageClick';
-
+    
     constructor(scene, x, y)
     {
         super(scene, x, y);
@@ -17,14 +17,14 @@ export class MainMenuButtons extends Phaser.GameObjects.Container
         this.buttonImage = scene.add.image(0, 0, this.buttonTexture);
         this.buttonImageMove = scene.add.image(0, 0, this.buttonTextureMove);
         this.buttonImageClick = scene.add.image(0, 0, this.buttonTextureClick);
-
+        
         this.add(this.buttonImage);
         this.add(this.buttonImageMove);
         this.add(this.buttonImageClick);
-
+        
         this.buttonImageMove.setVisible(false);
         this.buttonImageClick.setVisible(false);
-
+        
         this.setSize(this.buttonImage.width, this.buttonImage.height);
 
         this.setInteractive()
@@ -37,7 +37,84 @@ export class MainMenuButtons extends Phaser.GameObjects.Container
                 this.buttonImage.setVisible(true);
                 this.buttonImageMove.setVisible(false);
                 console.log('Phaser.Input.Events.GAMEOBJECT_POINTER_OUT');
+            })
+            .on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
+                this.buttonImage.setVisible(false);
+                this.buttonImageMove.setVisible(false);
+                this.buttonImageClick.setVisible(true);
+                console.log('Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN');
             });
+    }
+}
 
+export class ButtonNewGame extends MainMenuButtons
+{
+    buttonText = 'Новая игра';
+
+    constructor(scene, x, y)
+    {
+        super(scene, x, y);
+
+        this.text = scene.add.text(0, 0, this.buttonText, { fontFamily: 'Arial Black', fontSize: 12, color: '#ffffff' }).setOrigin(0.5);
+
+        this.add(this.text);        
+
+        this.setInteractive().on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
+            console.log('ButtonNewGame');
+        });
+    }
+}
+
+export class ButtonContinueGame extends MainMenuButtons
+{
+    buttonText = 'Продолжить игру';
+
+    constructor(scene, x, y)
+    {
+        super(scene, x, y);
+
+        this.text = scene.add.text(0, 0, this.buttonText, { fontFamily: 'Arial Black', fontSize: 12, color: '#ffffff' }).setOrigin(0.5);
+
+        this.add(this.text);
+
+        this.setInteractive().on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
+            console.log('ButtonContinueGame');
+        });
+    }
+}
+
+export class ButtonGameSettings extends MainMenuButtons
+{
+    buttonText = 'Настройки';
+
+    constructor(scene, x, y)
+    {
+        super(scene, x, y);
+
+        this.text = scene.add.text(0, 0, this.buttonText, { fontFamily: 'Arial Black', fontSize: 12, color: '#ffffff' }).setOrigin(0.5);
+
+        this.add(this.text);
+
+        this.setInteractive().on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
+            console.log('ButtonGameSettings');
+        });
+    }
+}
+
+export class ButtonGameExit extends MainMenuButtons
+{
+    buttonText = 'Выход';
+
+    constructor(scene, x, y)
+    {
+        super(scene, x, y);
+
+        this.text = scene.add.text(0, 0, this.buttonText, { fontFamily: 'Arial Black', fontSize: 12, color: '#ffffff' }).setOrigin(0.5);
+
+        this.add(this.text);
+
+        this.setInteractive().on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
+            console.log('ButtonGameExit');
+        });
     }
 }
